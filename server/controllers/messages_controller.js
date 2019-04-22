d1f1fd954ea80849dc5c5383a4aa07f296243225
User-1 <user-1@MacBook-28.local>
2019-04-22 16:05:35 -0600

    finished

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -18,9 +18,25 @@ module.exports = {
     },
 
     update: (req, res) => {
-
+        for(var i =0; i < messages.length; i++){
+            if(messages[i].id == req.params.id){
+                messages[i] = {
+                    id: messages[i].id,
+                    text: req.body.text || messages[i].text,
+                    time: messages[i].time
+                }
+            }
+        }
+        res.status(200).send(messages);
     },
     delete: (req, res) => {
-
+        console.log(messages);
+        for(var i = 0; i < messages.length; i++){
+            if(messages[i].id == req.params.id){
+                messages.splice(i, 1);
+            }
+        }
+        console.log(messages);
+        res.status(200).send(messages);
     },
 }
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const mc =  require('./controllers/messages_controller.js');
 
 const app = express();
 
 // parse JSON from the body
 app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
-const port = 3000;
+app.get('/api/messages', mc.read);
+app.post('/api/messages', mc.create);
+app.put('/api/messages/:id', mc.update);
+app.delete('/api/messages/:id', mc.delete);
+
+const port = 3001;
 app.listen(port, () =>{
     console.log(`listening on port ${ port }`)
 })
